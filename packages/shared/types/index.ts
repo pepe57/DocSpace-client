b980c3c760ec086a69268afac06b0cc2ca7f9e07
Viktor Fomin <viktor.fomin@onlyoffice.com>
2026-01-21 16:57:21 +0300

    Shared: Types: add default template item type

--- packages/shared/types/index.ts
+++ packages/shared/types/index.ts
@@ -341,3 +341,11 @@ declare global {
   export type SeparatorType =
     import("../components/context-menu/ContextMenu.types").SeparatorType;
 }
+
+export type TDefaultTemplateItem = {
+  id: number | null;
+  title?: string;
+  isModified?: boolean;
+  lastModified?: string;
+  extension: string;
+};
