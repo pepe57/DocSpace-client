fbb3dd125780dcdf9846076a0a6b0658b6e0deb4
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2026-01-28 16:45:14 +0100

    StatusMessage: Update imports

--- packages/shared/pages/backup/auto-backup/index.tsx
+++ packages/shared/pages/backup/auto-backup/index.tsx
@@ -40,7 +40,7 @@ import { OPERATIONS_NAME } from "../../../constants";
 import { ToggleButton } from "@docspace/ui-kit/components/toggle-button";
 import { getBackupStorage } from "../../../api/settings";
 import AutoBackupLoader from "../../../skeletons/backup/AutoBackup";
-import StatusMessage from "../../../components/status-message";
+import StatusMessage from "@docspace/ui-kit/components/status-message";
 import SocketHelper, {
   SocketEvents,
   type TSocketListener,
--- packages/shared/pages/backup/manual-backup/index.tsx
+++ packages/shared/pages/backup/manual-backup/index.tsx
@@ -38,7 +38,7 @@ import { RadioButton } from "@docspace/ui-kit/components/radio-button";
 import { startBackup } from "../../../api/portal";
 import { toastr } from "../../../components/toast";
 import { BackupStorageLocalKey, BackupStorageType } from "../../../enums";
-import StatusMessage from "../../../components/status-message";
+import StatusMessage from "@docspace/ui-kit/components/status-message";
 import SocketHelper, {
   SocketEvents,
   TSocketListener,
