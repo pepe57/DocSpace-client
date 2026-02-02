c77fb896f386f13a5f00fec960a3e2d96dfa544e
Vlada Gazizova <vlada.gazizova@onlyoffice.com>
2026-02-02 23:25:21 +0300

    Client:EditRoomGroupsDialog:Change roomIcon initial state from object with SVG data and id to string "folder", update coverId calculation to handle string roomIcon by returning the string value directly when roomIcon is a string type.

--- packages/client/src/components/dialogs/EditRoomGroupsDialog/sub-components/GroupIconDialog.tsx
+++ packages/client/src/components/dialogs/EditRoomGroupsDialog/sub-components/GroupIconDialog.tsx
@@ -69,10 +69,7 @@ const GroupIconDialog = ({
 
   const [roomIcon, setRoomIcon] = React.useState<
     ICover | ILogo | string | null
-  >({
-    data: `<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M3 3a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-7a1 1 0 0 1-.832-.445L7.465 3H3zM.879 1.879A3 3 0 0 1 3 1h5a1 1 0 0 1 .832.445L10.535 4H17a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V4a3 3 0 0 1 .879-2.121z" fill="${currentColorScheme?.main?.accent}"/></svg>`,
-    id: "folder",
-  });
+  >("folder");
 
   const [groupName, setGroupName] = React.useState<string>("");
 
@@ -93,7 +90,11 @@ const GroupIconDialog = ({
   }, [editingGroupId, currentGroupName]);
 
   const coverId =
-    typeof roomIcon === "object" && roomIcon !== null ? roomIcon.id : "";
+    typeof roomIcon === "object" && roomIcon !== null
+      ? roomIcon.id
+      : typeof roomIcon === "string"
+        ? roomIcon
+        : "";
 
   const onChangeGroupName = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
