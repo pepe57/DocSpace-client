8352ef51adfa013d38098dfe98d172c11cc579e1
Viktor <viktor.fomin@onlyoffice.com>
2026-02-03 15:57:19 +0700

    Shared: Section: fix import

--- packages/shared/components/section/sub-components/SectionBody.tsx
+++ packages/shared/components/section/sub-components/SectionBody.tsx
@@ -27,7 +27,7 @@
 import React from "react";
 import classNames from "classnames";
 
-import { DragAndDrop } from "../../drag-and-drop";
+import { DragAndDrop } from "@docspace/ui-kit/components/drag-and-drop";
 
 import styles from "../Section.module.scss";
 import { SectionBodyProps } from "../Section.types";
