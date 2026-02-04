2b824aeec33ca9c6b6abe3aa1b9531f3cfeddeb3
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2026-02-04 15:13:48 +0400

    refactor(client): migrate InvitePanel and dialogs from moment to luxon

--- packages/client/src/components/dialogs/InviteQuotaWarningDialog/index.js
+++ packages/client/src/components/dialogs/InviteQuotaWarningDialog/index.js
@@ -28,7 +28,7 @@ import React, { useState, useEffect } from "react";
 import { inject, observer } from "mobx-react";
 import { withTranslation, Trans } from "react-i18next";
 import { useNavigate, useLocation } from "react-router";
-import moment from "moment-timezone";
+import { parseToDateTime, formatDateLocalized } from "@docspace/shared/utils/date";
 import { ModalDialog } from "@docspace/shared/components/modal-dialog";
 import { Button } from "@docspace/shared/components/button";
 import { Text } from "@docspace/shared/components/text";
@@ -62,22 +62,19 @@ const InviteQuotaWarningDialog = (props) => {
   const { fromDate, byDate, delayDaysCount } = datesData;
 
   const gracePeriodDays = () => {
-    const fromDateMoment = moment(dueDate);
-    const byDateMoment = moment(delayDueDate);
+    const fromDateDt = parseToDateTime(dueDate);
+    const byDateDt = parseToDateTime(delayDueDate);
 
     setDatesData({
-      fromDate: fromDateMoment.format("LL"),
-      byDate: byDateMoment.format("LL"),
-      delayDaysCount: getDaysRemaining(byDateMoment),
+      fromDate: fromDateDt ? formatDateLocalized(fromDateDt, "DATE_MED", { locale: language }) : "",
+      byDate: byDateDt ? formatDateLocalized(byDateDt, "DATE_MED", { locale: language }) : "",
+      delayDaysCount: getDaysRemaining(byDateDt),
     });
   };
 
   useEffect(() => {
-    moment.locale(language);
-    if (window.timezone) moment().tz(window.timezone);
-
     gracePeriodDays();
-  }, [language, window.timezone]);
+  }, [language, dueDate, delayDueDate]);
 
   const onClose = () => {
     if (!isGracePeriod) {
