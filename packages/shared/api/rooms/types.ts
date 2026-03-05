f45912602c5719b61ac5360526db10cc311734c0
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2026-03-05 10:41:29 +0100

    Remove types duplicates

--- packages/shared/api/rooms/types.ts
+++ packages/shared/api/rooms/types.ts
@@ -25,6 +25,7 @@
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
 import type { TLogo } from "@docspace/ui-kit/types";
+import type { TAIRoomChatSettings } from "@docspace/ui-kit/types/ai";
 
 import type { TFile, TFolder, TShareSettings } from "../files/types";
 import type {
@@ -42,6 +43,8 @@ import type {
 } from "../../types";
 import type { TUser } from "../people/types";
 import type { TGroup } from "../groups/types";
+
+export type { TAIRoomChatSettings };
 
 export type TRoomSecurity = {
   ChangeOwner: boolean;
@@ -85,17 +88,6 @@ export type TWatermark = {
   scale?: number;
   image?: File | string;
   isImage?: boolean;
-};
-
-export type TMultimodal = { image: { formats: string[] } };
-
-export type TAIRoomChatSettings = {
-  prompt: string;
-  providerId: number;
-  modelId: string;
-  internal: boolean;
-  modelAlias?: string;
-  multimodal?: TMultimodal;
 };
 
 export type TRoom = {
