5bcdce14512b0f086ec3fb8a6d92091b7724dbce
Alexander Evdokimov <aleksandr.evdokimov@onlyoffice.com>
2026-03-20 11:56:23 +0300

    Shared: MediaViewer: add plugin viewer support to media viewer component with drag gestures

---
+++ packages/shared/components/media-viewer/sub-components/PluginViewer/PluginViewer.props.ts
@@ -0,0 +1,13 @@
+type PluginViewerProps = {
+  pluginViewerContent: React.ReactNode;
+  handleMaskClick: () => void;
+  onNext?: () => void;
+  onPrev?: () => void;
+  isFirstItem?: boolean;
+  isLastItem?: boolean;
+  devices?: {
+    isMobileOnly: boolean;
+    isMobile: boolean;
+    isDesktop: boolean;
+  };
+};
