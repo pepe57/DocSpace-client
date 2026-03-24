5d7f61988c7d63e2f672a4e04c07349dd7017370
Ik-Akmal <isomadinovakmal1997@gmail.com>
2026-03-25 00:08:46 +0500

    Remove unused popupRef from useCloseOnAnchorCovered in TagManagementPopup

--- packages/shared/components/tag-management/TagManagement.popup.tsx
+++ packages/shared/components/tag-management/TagManagement.popup.tsx
@@ -81,7 +81,6 @@ export const TagManagementPopup: React.FC<TagManagementPopupProps> = ({
 
   useCloseOnAnchorCovered({
     anchorRef: anchor,
-    popupRef: ref,
     onClose,
     enabled: !isMobile,
   });
