9fe4eb620da4889c58d198844457448b8110495c
gopienkonikita <nikita.gopienko@onlyoffice.com>
2026-05-18 13:42:00 +0300

    Doceditor: exclude @onlyoffice/docspace-api-sdk from server bundle via webpack externals

--- packages/doceditor/next.config.js
+++ packages/doceditor/next.config.js
@@ -106,8 +106,26 @@ if (isDev) {
 }
 
 module.exports = withBundleAnalyzer({
-  webpack(config) {
+  webpack(config, { isServer }) {
     const isProduction = config.mode === productionMode;
+
+    if (isServer) {
+      const existingExternals = Array.isArray(config.externals)
+        ? config.externals
+        : config.externals
+          ? [config.externals]
+          : [];
+      config.externals = [
+        ...existingExternals,
+        ({ request }, callback) => {
+          if (request === "@onlyoffice/docspace-api-sdk") {
+            return callback(null, `commonjs ${request}`);
+          }
+          callback();
+        },
+      ];
+    }
+
     // Add resolve configuration for shared package
     config.resolve = {
       ...config.resolve,
