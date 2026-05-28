f007bc1c6aeff26848386a26f3df447f8a926629
Alexey Safronov <alexey.safronov@onlyoffice.com>
2026-05-28 15:09:32 +0400

    Move social auth welcome from bar to empty view panel
    Relocate SocialAuthWelcomeBar from MainBar into EmptyViewContainer
    as SocialAuthWelcomePanel, shown only on the empty rooms root page.
    Remove the bar variant and clean up Bar.js dependencies accordingly.

--- packages/client/src/components/EmptyContainer/sub-components/EmptyViewContainer/SocialAuthWelcomePanel.jsx
+++ packages/client/src/components/EmptyContainer/sub-components/EmptyViewContainer/SocialAuthWelcomePanel.jsx
@@ -39,10 +39,10 @@ import { withTranslation } from "react-i18next";
 import { ColumnarInfoBar } from "@docspace/ui-kit/components/columnar-info-bar";
 import { Link, LinkType } from "@docspace/ui-kit/components/link";
 import { getBrandName } from "@docspace/shared/constants/brands";
+import PencilSvgUrl from "PUBLIC_DIR/images/pencil.outline.react.svg?url";
 
-const SocialAuthWelcomeBar = ({
+const SocialAuthWelcomePanel = ({
   t,
-  onLoad,
   onClose,
   tenantAlias,
   baseDomain,
@@ -61,15 +61,22 @@ const SocialAuthWelcomeBar = ({
       value: (
         <>
           <span>{domain}</span>
-          <Link
-            isHovered
-            type={LinkType.page}
-            fontSize="12px"
-            fontWeight="400"
+          <button
+            type="button"
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+              padding: 0,
+              flexShrink: 0,
+            }}
             onClick={onChangeDomainClick}
+            aria-label={t("Common:ChangeButton")}
           >
-            {t("Common:ChangeButton")}
-          </Link>
+            <img src={PencilSvgUrl} width="12" height="12" alt="" />
+          </button>
         </>
       ),
     },
@@ -84,20 +91,17 @@ const SocialAuthWelcomeBar = ({
       value: <span>{user?.email}</span>,
     },
     {
-      label: t("SocialAuthWelcomeDialog:GeneratedPassword"),
+      label: t("Common:Password"),
       value: (
-        <>
-          <span>{"*".repeat(8)}</span>
-          <Link
-            isHovered
-            type={LinkType.page}
-            fontSize="12px"
-            fontWeight="400"
-            onClick={onChangePasswordClick}
-          >
-            {t("Common:ChangeButton")}
-          </Link>
-        </>
+        <Link
+          type={LinkType.page}
+          fontSize="13px"
+          fontWeight="600"
+          textDecoration="underline dashed"
+          onClick={onChangePasswordClick}
+        >
+          {t("SocialAuthWelcomeDialog:SetEmailPassword")}
+        </Link>
       ),
     },
   ];
@@ -107,12 +111,11 @@ const SocialAuthWelcomeBar = ({
       headerText={t("SocialAuthWelcomeDialog:YourProfileDetails")}
       columns={columns}
       onAction={onClose}
-      onLoad={onLoad}
+      variant="page"
     />
   );
 };
 
 export default withTranslation(["SocialAuthWelcomeDialog", "Common"])(
-  SocialAuthWelcomeBar,
+  SocialAuthWelcomePanel,
 );
-
